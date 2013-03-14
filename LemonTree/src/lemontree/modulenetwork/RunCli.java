559aa79a6c25e2fbf3612675efdbb6b975d89261
eric.d.bonnet <eric.d.bonnet@2882c48c-5757-b95b-4e2e-1fc565505837>
2013-03-14 15:08:23 +0000

    version number change

--- LemonTree/src/lemontree/modulenetwork/RunCli.java
+++ LemonTree/src/lemontree/modulenetwork/RunCli.java
@@ -596,8 +596,8 @@ public class RunCli {
 	
 	public static void printBanner () {
 		System.out.println("");
-		System.out.println("LemonTree - Version 3.0");
-		System.out.println("-----------------------");
+		System.out.println("LemonTree - Version 3.0.1");
+		System.out.println("-------------------------");
 		System.out.println("");
 	}
 }
