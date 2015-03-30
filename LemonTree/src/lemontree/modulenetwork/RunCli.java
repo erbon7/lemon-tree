d9855a63141f97cdaab67d0666ffff067c2d2fd8
Eric Bonnet <eric.d.bonnet@gmail.com>
2015-03-30 11:33:02 +0200

    Update banner version number.

--- LemonTree/src/lemontree/modulenetwork/RunCli.java
+++ LemonTree/src/lemontree/modulenetwork/RunCli.java
@@ -629,7 +629,7 @@ public class RunCli {
 	
 	public static void printBanner () {
 		System.out.println("");
-		System.out.println("LemonTree - Version 3.0.2");
+		System.out.println("LemonTree - Version 3.0.3");
 		System.out.println("-------------------------");
 		System.out.println("");
 	}
