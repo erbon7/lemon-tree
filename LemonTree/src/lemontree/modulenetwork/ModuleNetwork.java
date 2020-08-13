b06cae6a0d21d89067cd7babe9aac3a552408388
Pau Erola <pauerola@gmail.com>
2020-08-13 13:06:50 +0100

    runtime error v3.1.0
    runtime error v3.1.0

--- LemonTree/src/lemontree/modulenetwork/ModuleNetwork.java
+++ LemonTree/src/lemontree/modulenetwork/ModuleNetwork.java
@@ -557,7 +557,7 @@ public class ModuleNetwork {
 
 		// revamp: added for compatibility with other functions 
 		this.condition_weight = new double[numCond];
-		for (int i = 0; i < numCond; i++)
+		for (int i = 0; i < numCond; i++) {
 			this.condition_weight[i] = 1.0;
 		}
 
